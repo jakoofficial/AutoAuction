feed3ecfdd8b3665f975de91b4907a76e4b8c021
LavenderPixl <annajacobsen1999@hotmail.com>
2023-09-11 10:51:14 +0200

    G1, G2, working on G3. CreateUserView created. HomeScreenView created. LoginView created.

---
+++ AutoAuction/Views/CreateUserView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AutoAuction.Views;
+
+public partial class CreateUserView : UserControl
+{
+    public CreateUserView()
+    {
+        InitializeComponent();
+    }
+}
