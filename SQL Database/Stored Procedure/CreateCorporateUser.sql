440a5f838e35f3aa43cb95205ffd72dcd2444f50
Matkip0 <biegel987@gmail.com>
2023-09-27 12:15:24 +0200

    Updated CreateCorporateUser

--- SQL Database/Stored Procedure/CreateCorporateUser.sql
+++ SQL Database/Stored Procedure/CreateCorporateUser.sql
@@ -3,8 +3,8 @@ as
 
 EXEC ('CreateUser ''' + @username + ''', ''' + @password + ''' ')
 
-INSERT INTO UserTable (Username, CorporateUser, Balance)
-VALUES (@username, 1, 0)
+INSERT INTO UserTable (Username, CorporateUser, Balance, Credit)
+VALUES (@username, 1, 0, 0)
 
 INSERT INTO CorporateUser (Username, CVRNumber)
 VALUES (@username, @CVRNumber)
