6e70aa40e103ba9df585d480291f26f2fd858907
Matkip0 <biegel987@gmail.com>
2023-09-28 10:34:04 +0200

    Added throw exception to CreateCorporateUser.sql and CreatePrivateUser.sql

--- SQL Database/Stored Procedure/CreateCorporateUser.sql
+++ SQL Database/Stored Procedure/CreateCorporateUser.sql
@@ -2,6 +2,16 @@ CREATE PROCEDURE CreateCorporateUser @username varchar(300), @password varchar(m
 as
 set xact_abort, nocount on;
 
+IF EXISTS(SELECT Username FROM UserTable WHERE Username = @username)
+BEGIN 
+	;THROW 99001, 'The username is already taken', 1;
+END
+
+IF EXISTS(SELECT CVRNumber FROM CorporateUser WHERE CVRNumber = @CVRNumber)
+BEGIN 
+	;THROW 99002, 'The CVR number is already in use', 1;
+END
+
 EXEC ('CreateLogin ''' + @username + ''', ''' + @password + ''' ')
 
 INSERT INTO UserTable (Username, CorporateUser, ZipCode, Balance, Credit)
--- SQL Database/Stored Procedure/CreatePrivateUser.sql
+++ SQL Database/Stored Procedure/CreatePrivateUser.sql
@@ -1,6 +1,17 @@
 CREATE PROCEDURE CreatePrivateUser @username varchar(300), @password varchar(max), @zipcode int, @CPRNumber int
 as
 set xact_abort, nocount on;
+
+IF EXISTS(SELECT Username FROM UserTable WHERE Username = @username)
+BEGIN 
+	;THROW 99001, 'The username is already taken', 1;
+END
+
+IF EXISTS(SELECT CPRNumber FROM PrivateUser WHERE CPRNumber = @CPRNumber)
+BEGIN 
+	;THROW 99002, 'The CPR number is already in use', 1;
+END
+
 EXEC ('CreateLogin ''' + @username + ''', ''' + @password + ''' ')
 
 INSERT INTO UserTable (Username, CorporateUser, ZipCode, Balance)
