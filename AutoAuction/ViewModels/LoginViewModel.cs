dd6fd52ba4bb3f0878460aae011336fe248faa20
Matkip <Matkip>
2023-09-29 10:25:34 +0200

    Added properties to loginViewmodel.cs

--- AutoAuction/ViewModels/LoginViewModel.cs
+++ AutoAuction/ViewModels/LoginViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,27 @@ namespace AutoAuction.ViewModels
 {
     public class LoginViewModel : ViewModelBase
     {
+        private string _username = "1";
+
+        public string Username
+        {
+            get => _username;
+            set => this.RaiseAndSetIfChanged(ref _username, value);
+        }
+
+        private string _password = "2";
+
+        public string Password
+        {
+            get => _password;
+            set => this.RaiseAndSetIfChanged(ref _password, value);
+        }
+
+        public void Login()
+        {
+            Debug.WriteLine("test");
+        }
+
+
     }
 }
