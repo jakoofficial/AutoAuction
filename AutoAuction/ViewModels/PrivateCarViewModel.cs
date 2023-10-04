aba3d21dcb085653882c7b8453ed5dd625fd08c1
Matkip <Matkip>
2023-10-05 00:10:27 +0200

    Added properties to PrivateCarViewModel.cs

--- AutoAuction/ViewModels/PrivateCarViewModel.cs
+++ AutoAuction/ViewModels/PrivateCarViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,39 @@ namespace AutoAuction.ViewModels
 {
     public class PrivateCarViewModel : ViewModelBase
     {
+        private string _height = "";
+        public string Height
+        {
+            get => _height;
+            set => this.RaiseAndSetIfChanged(ref _height, value);
+        }
+
+        private string _weight = "";
+        public string Weight
+        {
+            get => _weight;
+            set => this.RaiseAndSetIfChanged(ref _weight, value);
+        }
+
+        private string _depth = "";
+        public string Depth
+        {
+            get => _depth;
+            set => this.RaiseAndSetIfChanged(ref _depth, value);
+        }
+
+        private string _numberOfSeats = "";
+        public string NumberOfSeats
+        {
+            get => _numberOfSeats;
+            set => this.RaiseAndSetIfChanged(ref _numberOfSeats, value);
+        }
+
+        private bool _hasIsofix;
+        public bool HasIsofix
+        {
+            get => _hasIsofix;
+            set => this.RaiseAndSetIfChanged(ref _hasIsofix, value);
+        }
     }
 }
